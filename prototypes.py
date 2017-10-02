26f24a8547dc45831b88ec2fe70471d722544a9d
lbias <306508354@qq.com>
2017-10-02 21:34:43 +0800

    set site_pages_directory for ptototype templates

--- prototypes.py
+++ prototypes.py
@@ -2,6 +2,8 @@ import sys
 
 from django.conf import settings
 
+BASE_DIR = os.path.dirname(__file__)
+
 settings.configure(
     DEBUG=True,
     SECRET_KEY='f#&o4)9ibe=k1ofu696+4rt8#5e73fsf9leuj5@-&$nf4%(#jn',
@@ -12,6 +14,7 @@ settings.configure(
         'sitebuilder',
     ),
     STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
 )
 
 if __name__ == "__main__":
